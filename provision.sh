92ef8707b14ca365aef1d94370c52d45a66500e5
garbagetown <garbagetown@gmail.com>
2014-03-06 08:47:20 +0900

    modify provision.sh and add proxy role to work behind proxy. refs #4

--- provision.sh
+++ provision.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+while read line
+do
+  if [[ $line =~ ^http_proxy:.+$ ]]; then
+    arr=($line)
+    export http_proxy=${arr[1]}
+  fi
+done < /vagrant/ansible/roles/proxy/vars/main.yml
+
 if ! [ `which ansible` ]; then
-	apt-get update -y
-	apt-get install -y ansible
+  apt-get update -y
+  apt-get install -y ansible
 fi
 
 ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/playbook.yml
