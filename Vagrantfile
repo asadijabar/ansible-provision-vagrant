4237d26580b2cd12acb07e3db90ae948438f46e2
garbagetown <garbagetown@gmail.com>
2014-03-11 18:52:16 +0900

    modify to install ruby vagrant locally. closes #13

--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,6 @@ Vagrant.configure("2") do |config|
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
 
   config.vm.network :private_network, ip: "192.168.33.10"
-  config.vm.provision "shell", :path => "provision.sh"
-  config.vm.synced_folder ".", "/vagrant", :mount_options => ['dmode=775', 'fmode=664']
+  config.vm.provision "shell", path: "provision.sh", privileged: false
+  config.vm.synced_folder ".", "/vagrant", mount_options: ['dmode=775', 'fmode=664']
 end
--- provision.sh
+++ provision.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
+config=/vagrant/ansible/roles/proxy/vars/main.yml
+sed -i -e 's/\r$//' $config
+
 while read line
 do
   if [[ $line =~ ^http_proxy:.+$ ]]; then
     arr=($line)
-    export http_proxy=${arr[1]}
+    http_proxy=${arr[1]}
   fi
-done < /vagrant/ansible/roles/proxy/vars/main.yml
+done < $config
+
+apt_conf="/etc/apt/apt.conf.d/80proxy"
+
+if [ -e $apt_conf ]; then
+  sudo rm $apt_conf
+fi
+if [ -n $http_proxy ]; then
+  sudo touch $apt_conf
+  echo "Acquire::http::Proxy \"${http_proxy}\";" | sudo tee -a $apt_conf
+fi
 
 if ! [ `which ansible` ]; then
-  apt-get update -y
-  apt-get install -y ansible
+  sudo apt-get update -y
+  sudo apt-get install -y ansible
 fi
 
 ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/playbook.yml
